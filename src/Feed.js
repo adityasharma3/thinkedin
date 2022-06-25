7492b1b62e3bf446ee45b6954993fb98e4ddd198
Aditya Sharma <aditya.sharma9301@gmail.com>
2022-06-25 20:18:39 +0530

    added firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create'
 import InputOption from './InputOption';
@@ -6,8 +6,33 @@ import ImageIcon from '@material-ui/icons/Image'
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import Post from './Post';
+import db from './firebase';
 
 const Feed = () => {
+
+    const [input, setInput] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot((snapshot) => (
+            setPosts(snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+            }))
+            )))
+    }, []);
+
+    const sendPost = (e) => {
+        e.preventDefault();
+        // setPosts()
+        db.collection("posts").add({
+            name: 'Aditya SHarma',
+            description: 'This is a firestore vala test',
+            message: input,
+        })
+    };
+
     return (
         <div className="feed">
             <div className="feed__inputContainer">
@@ -15,8 +40,8 @@ const Feed = () => {
                     <CreateIcon />
 
                     <form action="">
-                        <input type="text" />
-                        <button type='submit'>Send</button>
+                        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
 
@@ -28,6 +53,12 @@ const Feed = () => {
 
                 </div>
             </div>
+
+            {posts.map((post) => (
+                <Post />
+            ))}
+
+            <Post name="Aditya Sharma" description="test" message="WOW this works somehow" />
         </div>
     )
 }
